f4eedfdbfb29b9659158707eb745626abd44f94d
Shehab Lotfallah <shehabw126@gmail.com>
2023-08-22 22:19:03 +0300

    Add project files.

---
+++ Task01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 4, num2 = 6;
+
+            Swap(ref num1, ref num2);
+
+            Console.WriteLine(value: num1);
+            Console.WriteLine(value: num2);
+        }
+
+        // Ex ----> Pass by Ref For Ref Type 
+
+        static void Swap(ref int x, ref int y) => (y, x) = (x, y);
+    }
+}
